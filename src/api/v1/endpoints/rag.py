7a5a25ec3d23a8e448740c8c00b3407911c047bd
Иванов Григорий <grigorashekivanov2@gmail.com>
2026-05-14 11:21:06 +0300

    unification endpoint responses

--- src/api/v1/endpoints/rag.py
+++ src/api/v1/endpoints/rag.py
@@ -2,18 +2,17 @@ from fastapi import APIRouter
 from fastapi import Depends
 
 from schemas.rag import RequestRunRag, ResponseRunRag
+from schemas.response import APIResponse, ok
 from services.rag import RagService
 
 from depends import get_rag_service
 
-router = APIRouter(prefix="/rag", tags=["RAG"])
+router = APIRouter(prefix="/rag")
 
-@router.post("/run", response_model=ResponseRunRag)
+@router.post("/run", response_model=APIResponse[ResponseRunRag])
 async def rag(
     request: RequestRunRag,
     rag_service: RagService = Depends(get_rag_service),
 ) -> ResponseRunRag:
 
-    return await rag_service.execute(
-        request
-    )
+    return ok(await rag_service.execute(request))
