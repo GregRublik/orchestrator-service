f7a4ca2cb90c20b8bc72f9fa911c9f7deeef027c
Иванов Григорий <grigorashekivanov2@gmail.com>
2026-05-29 14:21:05 +0300

    fix session manager

--- src/utils/session_manager.py
+++ src/utils/session_manager.py
@@ -1,4 +1,4 @@
-from aiohttp import ClientSession
+from aiohttp import ClientSession, TCPConnector
 
 
 class SessionManager:
@@ -8,7 +8,8 @@ class SessionManager:
     async def get_session(cls) -> ClientSession:
         """Возвращает сессию aiohttp, создавая её при первом вызове."""
         if cls._session is None or cls._session.closed:
-            cls._session = ClientSession()
+            connector = TCPConnector(ttl_dns_cache=30)  # кеш DNS на 30 секунд
+            cls._session = ClientSession(connector=connector)
         return cls._session
 
     @classmethod
